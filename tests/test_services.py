49d075e38224b74776f3ef4608c280636f33e907
Sylvain Bellemare <sbellem@gmail.com>
2016-06-01 16:14:52 +0200

    Parameterize bitcoin paramters (#33)
    and use different values

--- tests/test_services.py
+++ tests/test_services.py
@@ -24,8 +24,11 @@ def test_bitcoin_service_init_testnet():
 
 def test_make_request():
     from transactions.services.daemonservice import BitcoinDaemonService
-    s = BitcoinDaemonService(
-        'a', 'b', os.environ.get('BITCOIN_HOST', 'localhost'), 18332)
+    host = os.environ.get('BITCOIN_HOST', 'localhost')
+    rpcuser = os.environ.get('BITCOIN_RPCUSER', 'merlin')
+    rpcpassword = os.environ.get('BITCOIN_RPCPASSWORD', 'secret')
+    port = os.environ.get('BITCOIN_PORT', 18332)
+    s = BitcoinDaemonService(rpcuser, rpcpassword, host, port)
     response = s.make_request('getinfo')
     assert 'id' in response
     assert 'error' in response
